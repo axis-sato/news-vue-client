19fe9bb4c9cfee629eb1e7e81b0c40ee4a2102bf
Masahiko Sato <c8112002@gmail.com>
2017-06-18 00:08:13 +0900

    Fetch articles

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -1,14 +1,14 @@
 <template>
   <div>
     <md-layout md-gutter="8">
-      <md-layout md-gutter v-for="n in 10" class="foo" md-flex md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33" md-flex-large="33">
-        <md-card>
+      <md-layout md-gutter v-for="article in articles" class="foo" md-flex md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33" md-flex-large="33">
+        <md-card class="card">
           <md-card-media md-ratio="16:9">
-            <img src="https://avatars3.githubusercontent.com/u/1905224?v=3&u=7d8ae04d1ed853b7c95b0bc140eb6da137602dc9&s=200" alt="People">
+            <img :src="article.thumbnail" alt="People">
           </md-card-media>
 
           <md-card-header>
-            <div class="md-title">Title goes here</div>
+            <div class="md-title">{{ article.title }}</div>
           </md-card-header>
 
           <md-card-actions>
@@ -17,7 +17,7 @@
             </md-button>
           </md-card-actions>
           <md-card-content>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio itaque ea, nostrum odio.
+            {{ article.body }}
           </md-card-content>
         </md-card>
       </md-layout>
@@ -27,6 +27,42 @@
 
 <script>
   export default {
-    name: 'top'
+    name: 'top',
+    data () {
+      return {
+        articles: [],
+        limit: 5,
+        offset: 0
+      }
+    },
+    created () {
+      this.fetchArticles()
+    },
+    methods: {
+      fetchArticles () {
+        console.log('fetch articles')
+        fetch(`http://localhost:3000/v1/articles?limit=${this.limit}&offset=${this.offset}`)
+          .then(response => response.json())
+          .then((json) => {
+            console.log(json)
+            const articles = json.articles
+            if (articles.length > 0) {
+              this.articles = this.articles.concat(articles)
+            }
+            this.limit = json.limit
+            this.offset = json.nextOffset
+            this.isNext = json.isNext
+          })
+      }
+    }
   }
 </script>
+
+<style>
+  .foo {
+    /*background: red;*/
+  }
+  .card {
+    width: 100%;
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 import Router from 'vue-router'
 import Top from '@/pages/Top'
+import 'whatwg-fetch'
 
 Vue.use(Router)
 Vue.use(VueMaterial)
