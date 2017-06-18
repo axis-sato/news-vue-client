67361636b12fe2974456e1501fce6ab6fcf70f1d
Masahiko Sato <c8112002@gmail.com>
2017-06-18 17:46:24 +0900

    Use md-whiteframe instead of md-card

--- src/pages/Top.vue
+++ src/pages/Top.vue
@@ -2,27 +2,23 @@
   <div>
     <md-layout md-gutter="16">
       <md-layout md-gutter v-for="article in articles" :key="article.id" class="foo" md-flex md-flex-xsmall="100" md-flex-small="50" md-flex-medium="33" md-flex-large="33">
-        <md-card class="article-card">
-          <md-card-media md-ratio="16:9">
-            <img :src="article.thumbnail" alt="Thumbnail">
-          </md-card-media>
 
-          <md-card-header>
-            <div class="md-title">{{ article.title }}</div>
-          </md-card-header>
+        <md-whiteframe  md-elevation="2" class="article-whiteframe">
+          <img :src="article.thumbnail" alt="Thumbnail">
 
-          <md-card-actions>
-            <md-button class="md-icon-button md-raised md-accent">
-              <md-icon>add</md-icon>
-            </md-button>
-          </md-card-actions>
-          <md-card-content>
-            {{ article.body }}
-          </md-card-content>
-        </md-card>
+          <div class="md-title">{{ article.title }}</div>
+          <md-button class="md-icon-button md-raised md-accent">
+            <md-icon>add</md-icon>
+          </md-button>
+          <div class="md-title">{{ article.body }}</div>
+          <md-chip v-for="tag in article.tags" :key="tag.id" class="tag-chip">
+            {{ tag.name }}
+          </md-chip>
+        </md-whiteframe>
       </md-layout>
     </md-layout>
 
+    <!-- more -->
     <md-button class="md-raised more-button" @click.native="more" v-if="isNext">more</md-button>
   </div>
 </template>
@@ -69,6 +65,15 @@
   .article-card {
     width: 100%;
     margin-bottom: 16px;
+  }
+  .article-whiteframe {
+    width: 100%;
+    margin-bottom: 16px;
+  }
+  .tag-chip {
+    /*display: block;*/
+    /*float: left;*/
+    /*width: 10px;*/
   }
   .more-button {
     display: block;
